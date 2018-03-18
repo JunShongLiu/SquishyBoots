84196d8830012e828c69a847a5abf444bdab2b7e
Xian Zhe Guo <v0i0b@anvil.ugrad.cs.ubc.ca>
2018-03-17 22:47:45 -0700

    fixed some errors, Heroes integrity constraint violated

--- SquishyBoots.sql
+++ SquishyBoots.sql
@@ -12,125 +12,125 @@ drop table Item;
 
 
 CREATE TABLE Player (
-	Username 	varchar(20) UNIQUE,
-	Email		varchar(20),
-	Player_ID	int,
-	PRIMARY KEY(Player_ID)
+    Username     varchar(80) UNIQUE,
+    Email        varchar(80),
+    Player_ID    int,
+    PRIMARY KEY(Player_ID)
 );
 
 grant select on Player to public;
 
 CREATE TABLE Location ( 
-	Loc_ID		int,
-	L_Name 		varchar(20),
-	City		varchar(20),
-	Island		varchar(20),
-	PRIMARY KEY(Loc_ID)
+    Loc_ID        int,
+    L_Name         varchar(80),
+    City        varchar(80),
+    Island        varchar(80),
+    PRIMARY KEY(Loc_ID)
 );
 
 grant select on Location to public;
 
 /* Cant add the ON UPDATE CASCADE */
 CREATE TABLE Quest ( 
-	Q_ID		int,
-	q_name		varchar(20), 
-	Loc_id		int not NULL,
-	Difficulty	int,
-	PRIMARY KEY(Q_ID),
-	FOREIGN KEY(Loc_id) REFERENCES Location ON DELETE CASCADE
+    Q_ID        int,
+    q_name        varchar(80), 
+    Loc_id        int not NULL,
+    Difficulty    int,
+    PRIMARY KEY(Q_ID),
+    FOREIGN KEY(Loc_id) REFERENCES Location ON DELETE CASCADE
 );
 
 grant select on Quest to public;
 
 CREATE TABLE Characters (
-	HP				int,
-	MP				int,
-	Char_Name 		varchar(20),
-	Char_Level		int,
-	Char_ID			int,
-	PRIMARY KEY(Char_id)
+    HP                int,
+    MP                int,
+    Char_Name         varchar(80),
+    Char_Level        int,
+    Char_ID            int,
+    PRIMARY KEY(Char_id)
 );
 
 grant select on Characters to public;
 
 CREATE TABLE Enemy (
-	Enemy_Level		int,
-	Enemy_Exp		int,
-	Char_ID			int,
-	PRIMARY KEY(Char_ID),
-	FOREIGN KEY(Char_ID) REFERENCES Characters
+    Enemy_Level        int,
+    Enemy_Exp        int,
+    Char_ID            int,
+    PRIMARY KEY(Char_ID),
+    FOREIGN KEY(Char_ID) REFERENCES Characters
 );
 
 grant select on Enemy to public;
 
 CREATE TABLE Hero (
-	Hero_Class					varchar(20),
-	Job							varchar(20),
-	Quests_Completed			int,
-	Player_ID					int,
-	Char_ID						int,
-	PRIMARY KEY(Char_ID),
-	FOREIGN KEY(Char_ID) REFERENCES Characters,
-	FOREIGN KEY(Player_ID) REFERENCES Player
+    Hero_Class                    varchar(80),
+    Job                            varchar(80),
+    Quests_Completed            int,
+    Player_ID                    int,
+    Char_ID                        int,
+    PRIMARY KEY(Char_ID),
+    FOREIGN KEY(Char_ID) REFERENCES Characters,
+    FOREIGN KEY(Player_ID) REFERENCES Player
 );
 
 grant select on Hero to public;
 
 CREATE TABLE Item(
-	Item_ID			int,
-	I_Level			int,
-	I_Type			varchar(20),
-	I_Name			varchar(20),
-	I_Value			int,
-	PRIMARY KEY(Item_ID)
+    Item_ID            int,
+    I_Level            int,
+    I_Type            varchar(80),
+    I_Name            varchar(80),
+    I_Value            int,
+    PRIMARY KEY(Item_ID)
 );
 
 grant select on Item to public;
 
 CREATE TABLE Carries(
-	Char_id		int,
-	Item_id		int,
-	PRIMARY KEY(Char_id, Item_id),
-	FOREIGN KEY(Char_id) REFERENCES Hero,
-	FOREIGN KEY(Item_id) REFERENCES Item
+    Char_id        int,
+    Item_id        int,
+    PRIMARY KEY(Char_id, Item_id),
+    FOREIGN KEY(Char_id) REFERENCES Hero,
+    FOREIGN KEY(Item_id) REFERENCES Item
 );
 
 grant select on Carries to public;
 
 CREATE TABLE Completes(
-	Char_id		int,
-	Q_id		int,
-	PRIMARY KEY(Char_id, Q_id),
-	FOREIGN KEY(Char_id) REFERENCES Hero,
-	FOREIGN KEY(Q_id) REFERENCES Quest
+    Char_id        int,
+    Q_id        int,
+    PRIMARY KEY(Char_id, Q_id),
+    FOREIGN KEY(Char_id) REFERENCES Hero,
+    FOREIGN KEY(Q_id) REFERENCES Quest
 );
 
 grant select on Completes to public;
 
 CREATE TABLE Has(
-	Enemy_id	int,
-	Q_id	 	int,
-	PRIMARY KEY(Enemy_id, Q_id),
-	FOREIGN KEY(Enemy_id) REFERENCES Enemy,
-	FOREIGN KEY(Q_id) REFERENCES Quest
+    Enemy_id    int,
+    Q_id         int,
+    PRIMARY KEY(Enemy_id, Q_id),
+    FOREIGN KEY(Enemy_id) REFERENCES Enemy,
+    FOREIGN KEY(Q_id) REFERENCES Quest
 );
 
 grant select on Has to public;
 
 insert into Player
-values('Antipater', 'jun@email.com', '1')
+values('Antipater', 'jun@email.com', 1)
 
 insert into Player
-values('Philippina', 'karen@email.com', '2')
+values('Philippina', 'karen@email.com', 2)
 
 insert into Player
-values('Xoel', 'silas@email.com', '3')
+values('Xoel', 'silas@email.com', 3)
 
 insert into Player
-values('Eadburga', 'marijke@email.com', '4')
+values('Eadburga', 'marijke@email.com', 4)
 
 insert into Player
-values('Jockie', 'matthias@email.com', '5')
+values('Jockie', 'matthias@email.com', 5)
 
 insert into Location
 values(1, 'Road of Regrets 1', 'Temple of Time', 'Ossyria');
@@ -163,70 +163,85 @@ insert into Quest
 values(5, 'Runaway Brother', 3, 3);
 
 insert into Characters
-values(2, 'Jake', 195, 75, 4);
+values(195, 75, 'Jake', 4, 1);
 
 insert into Characters
-values(4, 'David', 270, 20, 6);
+values(270, 20, 'David', 6, 2);
 
 insert into Characters
-values(3, 'Sally', 295, 10, 12);
+values(295, 10, 'Sally', 12, 3);
 
 insert into Characters
-values(1, 'Wolf', 200, 80, 10);
+values(295, 10, 'Daisy', 12, 4);
 
 insert into Characters
-values(5, 'Tino', 120, 250, 8);
+values(295, 10, 'Daisy', 12, 5);
+
+insert into Characters
+values(200, 80,'Wolf', 1, 6);
+
+insert into Characters
+values(120, 250, 'Tino', 8, 7);
+
+insert into Characters
+values(15, 0, 'Snail', 1, 8);
+
+insert into Characters
+values(35, 0, 'Stump', 4, 9);
+
+insert into Characters
+values(80, 10, 'Slime', 7, 10);
 
 insert into Enemy
-values(1, 0, 1);
+values(1, 0, 6);
 
 insert into Enemy
-values(6, 6, 4);
+values(8, 6, 7);
 
 insert into Enemy
-values(7, 12, 7);
+values(1, 12, 8);
 
 insert into Enemy
-values(8, 17, 10);
+values(4, 13, 9);
 
 insert into Enemy
-values(5, 3, 1);
+values(7, 17, 10);
 
 insert into Hero
-values(9, 2, 'Luminous', 4117);
+values('Magician', 'Luminous', 4117, 1, 5);
 
 insert into Hero
-values(2, 1, 'Kinesis', 1);
+values('Magician', 'Kinesis', 1, 2, 4);
 
 insert into Hero
-values(3, 5, 'Zero', 5490);
+values('Warrior', 'Zero', 5490, 3, 3);
 
 insert into Hero
-values(4, 3, 'Mercedes', 588);
+values('Bowman', 'Mercedes', 588, 4, 2);
 
 insert into Hero
-values(10, 4, 'Mechanic', 711);
+values('Pirate', 'Mechanic', 711, 5, 1);
 
 insert into Item
-values(1, 'Mithril Sharp Helm', 'hat', 22, 100);
+values(1, 22, 'hat', 'Mithril Sharp Helm', 100);
 
 insert into Item
-values(2, 'Squishy Shoes', 'shoes', 30, 200);
+values(2, 30, 'shoes', 'Squishy Shoes', 200);
 
 insert into Item
-values(3, 'Adamantium Igor', 'claws', 20, 150);
+values(3, 20, 'claws', 'Adamantium Igor', 150);
 
 insert into Item
-values(4, 'Reverse Blindness', 'gun', 120, 1000);
+values(4, 120, 'gun', 'Reverse Blindness', 1000);
 
 insert into Item
-values(5, 'Bamboo Spears', 'spear', 20, 90);
+values(5, 20, 'spear', 'Bamboo Spears', 90);
 
 insert into Carries
 values(3, 2);
 
 insert into Carries
-values(2, 2);
+values(2, 3);
 
 insert into Carries
 values(4, 4);
@@ -253,17 +268,17 @@ insert into Completes
 values(3, 5);
 
 insert into Has
-values(1, '(Wanted) Green Mushrroms');
+values(6, 1);
 
 insert into Has
-values(5, 'Path to the Past');
+values(7, 2);
 
 insert into Has
-values(1, 'Keeny’s Research on Neo Huroid!');
+values(8, 3);
 
 insert into Has
-values(5, 'Keeny’s Research on D.Roid!!');
+values(9, 4);
 
 insert into Has
-values(5, 'Runaway Brother');
+values(10, 5);
 
