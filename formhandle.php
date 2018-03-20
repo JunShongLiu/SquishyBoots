1293d1cf50a90ebeadaca96a47c41534a3b7e093
Hui Jia Chen <y0w0b@anvil.ugrad.cs.ubc.ca>
2018-03-20 11:36:27 -0700

    add file

---
+++ formhandle.php
@@ -0,0 +1,19 @@
+<!-- <DOCTYPE html>
+<html>
+	<head>
+		<title>
+			PHP Login Form using Sessions
+		</title>
+	</head>
+<body>
+
+<form action="form_handle.php" method="get">
+Name: <input type="text" name="name"><br>
+Email: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>  -->
+
+
