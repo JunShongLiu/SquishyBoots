b65bf33ee1fd2a5c33889f0780aa53239050aa6d
Hui Jia Chen <y0w0b@anvil.ugrad.cs.ubc.ca>
2018-03-20 15:57:34 -0700

    trying to do queries and heroes

--- formhandle.php
+++ formhandle.php
@@ -2,16 +2,30 @@
 <html>
 	<head>
 		<title>
-			PHP Login Form using Sessions
+			Selection & Projection
 		</title>
 	</head>
 <body>
 
-<form action="form_handle.php" method="get">
-Name: <input type="text" name="name"><br>
-Email: <input type="text" name="email"><br>
+<h2> Projection </h2>
+<form action="querydisplay.php" method="get">
+Table: <input type="text" name="table"><br>   
+Column: <input type="text" name="column1"><br>
+Column: <input type="text" name="column2"><br>
+
+<h2> Selection </h2>
+
+Row: <input type="text" name="row1"><br>
+Row: <input type="text" name="row2"><br>
+<form methods="POST" action="formhandle.php">
 <input type="submit">
+
 </form>
+
+<br> 
+<form methods="POST" action="formhandle.php">
+<p><input type="submit" value="Reset" name="reset"></p> </form>
+
 
 </body>
 </html> 
--- test.php
+++ test.php
@@ -3,11 +3,35 @@
 
 if ($c=OCILogon("ora_s4i0b", "a31112148", "dbhost.ugrad.cs.ubc.ca:1522/ug")) {
   echo "Successfully connected to Oracle.\n";
+
+$query = "SELECT * FROM Character"; 
+$result = oci_parse($db_conn, $query); 
+oci_execute($result);
+
+$class = isset($_POST['class']) == true ? $_POST['class'] : '';
+$job = isset($_POST['job']) == true ? $_POST['job'] : '';
+$charname = isset($_POST['charname']) == true ? $_POST['charname'] : '';
+
+echo $class;
+echo $job;
+echo $charname;
+
+while (($row = oci_fetch_array($result, OCI_ASSOC)) != false){
+    echo $row[0].\n; 
+
+}
+
   OCILogoff($c);
+
+
+
 } else {
   $err = OCIError();
   echo "Oracle Connect Error " . $err['message'];
 }
+
+
+
 
 ?>
 </html>
