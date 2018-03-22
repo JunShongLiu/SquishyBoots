017b259083dbb8144ddb3d041e7c5bd156410012
mrcavd <ca_vindeng@icloud.com>
2018-03-22 16:46:17 -0700

    Separate page for Join Query

--- join_query.php
+++ join_query.php
@@ -24,11 +24,67 @@ This is the analysis screen
 </div>
 
 <div class="container-fluid">
-  <h2>Admin's Analysis Screen</h2>
+<h2>Admin's Analysis Screen</h2>
 </div>
 
 <!-- Calvin's Button -->
-<p>Find the location where quest(s) of difficulty 0 is held</p>
-<form action="analysis_screen.php" method="GET" id="JoinForm">
+<p>Find the heroes name, class and job of each player</p>
+<form action="join_query.php" method="GET" id="JoinForm">
 <input type="submit" value="Execute Query" class="btn btn-primary" name="join">
 </form>
+
+</body>
+</html>
+
+<?php
+include("db_execute.php");
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = oci_connect("ora_d4p0b", "a53595154", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+if ($db_conn) {
+	echo "<script>console.log( 'DB Connected' );</script>";
+	if(array_key_exists('join', $_GET)){
+		//echo "<br> JOIN <br>";
+		echo "<script>console.log( 'Button Pressed' );</script>";
+		$_SESSION["Join_Query"] = "select player_id, char_name, hero_class, job from player p natural join hero h natural join characters c";
+		
+		session_write_close();
+		header("location: join_query.php");
+	}
+	else {
+		if(isset($_SESSION["Join_Query"])){
+			//echo "<br> JOIN <br>";
+			$query = $_SESSION['Join_Query'];
+			//$createView = executePlainSQL("create view heroCharacter (char_name, hero_class, job, player_id) AS
+			//				select char_name, hero_class, job, player_id from characters c natural join hero h
+			//				");
+			$result = executePlainSQL("select player_id, char_name, hero_class, job from player p natural join hero h natural join characters c");
+			OCICommit($db_conn);
+			echo "<br>Result from Join Query<br>";
+			echo $result[1];
+			echo "<table border = '1'>
+			<tr>
+			<th>Player ID</th>
+			<th>Character Name</th>
+			<th>Hero Class</th>
+			<th>Job</th>
+			</tr>";
+			//echo "<tr><th>Name</th></tr>";
+			while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+				//echo "<tr><td>" .$row["PLAYER_ID"] .", ". $row["CHAR_NAME"] ." , ". $row["HERO_CLASS"] .", ". $row["JOB"] . "</td></tr>";
+				//echo "$row[0] . $row[1] . $row[2] . $row[3] . $row[4] . $row[5]<br>"; 
+				echo"<tr>";
+				echo"<td>" . $row['PLAYER_ID'] . "</td>";
+				echo"<td>" . $row['CHAR_NAME'] . "</td>";
+				echo"<td>" . $row['HERO_CLASS'] . "</td>";
+				echo"<td>" . $row['JOB'] . "</td>";
+				echo"</tr>";
+			}
+			echo"</table>";
+			unset($_SESSION['Join_Query']);
+		}
+	}
+	
+}
+
+
+?>
